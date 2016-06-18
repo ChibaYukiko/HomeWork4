4785e2bb1a7ae6886b581192d58ce9ed1aef3df6
千葉友紀子 <ChibaYukiko@chibayukiko-no-MacBook-Air.local>
2016-06-19 00:03:14 +0900

    tochu

--- pageRank.cpp
+++ pageRank.cpp
@@ -4,20 +4,27 @@
 #include <map>
 #include <typeinfo>
 #include <list>
+#include <vector>
 
 using namespace std;
 
-//set <string> s;
 
-map < string, int > vertex; // < vertex, score >
-map < string, list<string> > node; // A -> B : < A,B > C -> A, C -> B : <C,[A,B]>
+class Vertex {
+public:
+  std::string name;  // このVertex
+  double score;  // 点数
+  std::vector<Vertex> neighbors;  // 矢印の先の隣接Vertexたち
+  double kari_score;  // もっと必要な情報があればここに入れる
+};
+
  
 int main(){
 
-  std::ifstream ifs("small_data.txt"); // file open
   string str;
   int ver_num = 0, node_num = 0;
-  
+  Vertex vertex[20];
+
+  std::ifstream ifs("small_data.txt"); // file open
   if (ifs.fail()){
     std::cerr << "失敗" << std::endl;
     return -1;
@@ -25,11 +32,15 @@ int main(){
   
   getline(ifs, str); // vertex count
   ver_num = atoi(str.c_str());
+  std::cout << ver_num << std::endl;
+
 
   for(int i = 0; i < ver_num; i++){ // give each vertex 100scores
     getline(ifs, str);
-    vertex.insert(pair<string, int>(str, 100));
-    std::cout << str << ":" << vertex[str] << std::endl;
+    vertex[i].name = str;
+    vertex[i].score = 100.0;
+    vertex[i].kari_score = 0;
+    //std::cout << vertex[i].name << ":" << vertex[i].score << std::endl;    
   }
 
   getline(ifs, str); //node count
@@ -40,20 +51,36 @@ int main(){
 
     getline(ifs,str,' ');
     vertex1 = str;
-    std::cout << vertex1 << std::endl;
+    //std::cout << vertex1 << std::endl;
 
     getline(ifs,str);
     vertex2 = str;
-    std::cout << vertex2 << std::endl;
+    //std::cout << vertex2 << std::endl;
 
-    if(node.find(vertex1) == node.end()){ // key != vertex1
-      
-    }else{ //vertex1 exist
-      
+
+    int ver = 0, edge = 0 ;
+    while( ver < ver_num ){
+      if(vertex[ver].name == vertex1){
+	break;
+      }else{
+	ver++;
+      }
     }
- 
-  }
 
+    while( edge < ver_num ){
+      if(vertex[edge].name == vertex2){
+	break;
+      }else{
+	edge++;
+      }
+    }
+    
+    vertex[ver].neighbors.push_back(vertex[edge]);
+
+    //std::cout << vertex[ver].name << "->" << vertex[ver].neighbors << std::endl;
+
+   
+   }
 
  
   //fclose(ifs);
