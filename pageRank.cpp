0125e13109b52cb00c6ac8d1a03f524f454cbdb4
千葉友紀子 <ChibaYukiko@chibayukiko-no-MacBook-Air.local>
2016-06-19 11:07:05 +0900

    make caluculator

--- pageRank.cpp
+++ pageRank.cpp
@@ -17,36 +17,44 @@ public:
   double kari_score;  // もっと必要な情報があればここに入れる
 };
 
- 
-int main(){
+std::ifstream ifs("small_data.txt"); // file open
+Vertex vertex[20];
+
+void shokika(int ver_num){
 
   string str;
-  int ver_num = 0, node_num = 0;
-  Vertex vertex[20];
-
-  std::ifstream ifs("small_data.txt"); // file open
-  if (ifs.fail()){
-    std::cerr << "失敗" << std::endl;
-    return -1;
-  }
   
-  getline(ifs, str); // vertex count
-  ver_num = atoi(str.c_str());
-  std::cout << ver_num << std::endl;
-
-
   for(int i = 0; i < ver_num; i++){ // give each vertex 100scores
     getline(ifs, str);
     vertex[i].name = str;
     vertex[i].score = 100.0;
-    vertex[i].kari_score = 0;
-    //std::cout << vertex[i].name << ":" << vertex[i].score << std::endl;    
+    vertex[i].kari_score = 0;  
+  }
+  
+}
+
+int search_vertex(string str,int edge_num){
+
+  int num = 0;
+  
+  while(num < edge_num){
+    if(vertex[num].name == str){
+      break;
+    }else{
+      num++;
+    }
   }
 
-  getline(ifs, str); //node count
-  node_num = atoi(str.c_str());
+  return num;
+  
+}
 
-  for(int j = 0; j < node_num; j++){
+
+void insert_neighbor(int edge_num){
+
+  string str;
+
+   for(int j = 0; j < edge_num; j++){
     string vertex1, vertex2;
 
     getline(ifs,str,' ');
@@ -59,30 +67,55 @@ int main(){
 
 
     int ver = 0, edge = 0 ;
-    while( ver < ver_num ){
-      if(vertex[ver].name == vertex1){
-	break;
-      }else{
-	ver++;
-      }
-    }
 
-    while( edge < ver_num ){
-      if(vertex[edge].name == vertex2){
-	break;
-      }else{
-	edge++;
-      }
-    }
+    ver = search_vertex(vertex1, edge_num);
+
+    edge = search_vertex(vertex2, edge_num);
     
     vertex[ver].neighbors.push_back(vertex[edge]);
 
-    //std::cout << vertex[ver].name << "->" << vertex[ver].neighbors << std::endl;
+    std::cout << vertex[ver].name << "->" << vertex[ver].neighbors.back().name << std::endl;
 
-   
    }
 
+}
  
+int main(){
+
+  string str;
+  int ver_num = 0, edge_num = 0;
+
+
+  if (ifs.fail()){
+    std::cerr << "失敗" << std::endl;
+    return -1;
+  }
+  
+  getline(ifs, str); // vertex count
+  ver_num = atoi(str.c_str());
+  std::cout << ver_num << std::endl;
+
+  shokika(ver_num);
+
+
+  getline(ifs, str); //node count
+  edge_num = atoi(str.c_str());
+
+  insert_neighbor(edge_num);
+
+
+  // 点数の計算
+  int score = vertex[j].score / vertex[j].neighbors.size();
+
+  for(int i = 0; i < vertex[j].neighbors.size(); i++){
+    vertex[j].neighbors[i].kari_score += score;
+  }
+
+  vertex[j].score = vertex[j].kari_score;
+    
+
+  
+  
   //fclose(ifs);
   return 0;
 }
