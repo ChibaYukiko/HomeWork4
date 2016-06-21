7c920a95e9983472842381f084c87669353e5c53
千葉友紀子 <ChibaYukiko@chibayukiko-no-MacBook-Air.local>
2016-06-21 17:47:57 +0900

    success

--- pageRank.cpp
+++ pageRank.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-#define SIZE 26 // 頂点の数
+#define SIZE 1000 // 頂点の数
 
 
 class Vertex {
@@ -15,14 +15,14 @@ public:
   string name;  // このVertex
   double score;  // 点数
   vector<Vertex*> neighbors;  // 矢印の先の隣接Vertexたち
-  double kari_score;  // 与えられた点数を代入する
+  double given_score;  // 与えられた点数を代入する
 };
 
 std::ifstream ifs("medium_data.txt"); // file open
 
 Vertex vertex[SIZE];
 
-void shokika(int ver_num){ // 初期化
+void shokika(int ver_num){ // 初期化 ver_num は頂点の数
 
   string str;
   
@@ -30,16 +30,16 @@ void shokika(int ver_num){ // 初期化
     getline(ifs, str);
     vertex[i].name = str;
     vertex[i].score = 100.0;
-    vertex[i].kari_score = 0.0;
+    vertex[i].given_score = 0.0;
   }
   
 }
 
-int search_vertex(string str,int edge_num){ //頂点を探す->そのindexを返す
+int search_vertex(string str,int ver_num){ //頂点を探す 引数は(頂点の名前と, 頂点の数)->そのindexを返す
 
   int num = 0;
   
-  while(num < edge_num){
+  while(num < ver_num){
     if(vertex[num].name == str){
       break;
     }else{
@@ -52,7 +52,7 @@ int search_vertex(string str,int edge_num){ //頂点を探す->そのindexを返
 }
 
 
-void insert_neighbor(int edge_num){ //　neighbors の代入
+void insert_neighbor(int ver_num, int edge_num){ //　neighbors の代入 引数は(頂点の数, 矢印の数)
 
   string str;
 
@@ -67,9 +67,9 @@ void insert_neighbor(int edge_num){ //　neighbors の代入
 
     int ver = 0, edge = 0 ; // if A -> C then ver: A's index edge: C's index
 
-    ver = search_vertex(vertex1, edge_num);
+    ver = search_vertex(vertex1, ver_num);
 
-    edge = search_vertex(vertex2, edge_num);
+    edge = search_vertex(vertex2, ver_num);
     
     vertex[ver].neighbors.insert(vertex[ver].neighbors.begin(), &vertex[edge]);
 
@@ -82,17 +82,21 @@ void pageRank(int ver_num){ // 点数配分の計算
   for(int i = 0; i < ver_num ; i++){
 
     int n = vertex[i].neighbors.size(); // お隣の頂点の数
-    double div_score = vertex[i].score / n; //自分の今の持ち点をつながっている矢印の数で割った点数
-
-    for(int j = 0; j < n ; j++){
-      vertex[i].neighbors[j]->kari_score += div_score; // お隣さんに点数を与える
-    }
     
+    if(n != 0){ //お隣さんがいたら点数配分する
+      double div_score = vertex[i].score / n; //自分の今の持ち点をつながっている矢印の数で割った点数
+
+      for(int j = 0; j < n ; j++){
+	vertex[i].neighbors[j]->given_score += div_score; // お隣さんに点数を与える
+      }
+    }else{
+      vertex[i].score = 0;
+    }
   }
 
-  for(int k = 0; k < ver_num; k++){ //与えられた点数を自分の点数として代入してkari_scoreはリセット
-    vertex[k].score = vertex[k].kari_score;
-    vertex[k].kari_score = 0.0;
+  for(int k = 0; k < ver_num; k++){ //与えられた点数を自分の点数として代入してgiven_scoreはリセット
+    vertex[k].score = vertex[k].given_score;
+    vertex[k].given_score = 0.0;
     std::cout << vertex[k].name << " : " << vertex[k].score << std::endl;
   }
   
@@ -105,41 +109,44 @@ int main(){
   int ver_num = 0, edge_num = 0;
 
 
-  if (ifs.fail()){
+  if (ifs.fail()){ // file open が失敗
     std::cerr << "失敗" << std::endl;
     return -1;
   }
   
   getline(ifs, str); // vertex count
   ver_num = atoi(str.c_str());
-  //std::cout << ver_num << std::endl;
 
-  shokika(ver_num);
+  shokika(ver_num);　//初期化
 
 
   getline(ifs, str); //node count
   edge_num = atoi(str.c_str());
 
-  insert_neighbor(edge_num);
+  insert_neighbor(ver_num, edge_num); //お隣さんを追加
 
 
-  for(int i = 1; i < 4; i++){ //pageRank を実行
-    std::cout << "for文 " << i << " 回目"<< std::endl;
+  for(int i = 1; i < ver_num; i++){ //pageRank を実行
+    std::cout << "for文 " << i+1 << " 回目"<< std::endl;
     pageRank(ver_num);
    
   }
 
-  double max_score = vertex[0].score;
-  string max_name = vertex[0].name;
+  double max_score = vertex[0].score; //最高点
+  string max_name = vertex[0].name; //最高点のvertexの名前
+  int total_score = vertex[0].score; //　全ての点の合計点
 
   for(int j = 1; j < ver_num ; j++){ // 最高点を探す
     if(max_score < vertex[j].score){
       max_score = vertex[j].score;
       max_name = vertex[j].name;
     }
+    total_score += vertex[j].score;
   }
 
   std::cout << "Max score is " << max_name << " : " << max_score << std::endl;
+  cout << "Total score : " << total_score << endl;
+  cout << "True value (Total score) : "<< 100.0*ver_num << endl;
   
   return 0;
 }
